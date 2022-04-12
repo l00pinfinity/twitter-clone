cb576dfa0ac8c6f144142cf65794800228dffdd3
l00pinfinity <collinskipkemboi001@gmail.com>
2022-04-12 10:24:01 +0300

    validate isignup and ilogin form

--- src/app/auth/flow/ilogin/ilogin.component.ts
+++ src/app/auth/flow/ilogin/ilogin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ilogin',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IloginComponent implements OnInit {
 
+  ilogin = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //on form submit
+  loginUser() {
+    console.log(this.ilogin.value);
   }
 
 }
--- src/app/auth/flow/isignup/isignup.component.ts
+++ src/app/auth/flow/isignup/isignup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-isignup',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./isignup.component.scss']
 })
 export class IsignupComponent implements OnInit {
+  isignup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  signinUser(){
+    console.log(this.isignup.value);
   }
 
 }
