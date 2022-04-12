31c6da3fe5e85956c17cff809f6422cfb6e7fefd
l00pinfinity <collinskipkemboi001@gmail.com>
2022-04-12 15:56:30 +0300

    center p to look good

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { tap } from 'rxjs/internal/operators/tap';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  isLoggedIn: boolean = false;
 
   constructor(private http: HttpClient, private router: Router) { }
 
